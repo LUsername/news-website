445055fef2961742c82559a39e618bcc1f089bb2
LUsername <492254446@qq.com>
2019-11-29 15:57:51 +0800

    完成列表页面的制作

--- src/containers/List/index.js
+++ src/containers/List/index.js
@@ -1,11 +1,27 @@
 import React,{Component} from 'react';
+import { List } from 'antd';
 
-class List extends Component{
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
+
+class NewList extends Component{
+
+
     render(){
         return(
-            <div>List</div>
+            <List
+                style={{background:'#fff'}}
+                bordered
+                dataSource={data}
+                renderItem={item => <List.Item>{item}</List.Item>}
+            />
         )
     }
 }
 
-export default List;
+export default NewList;
