cd12e73223a6bd8587458a3a494e25d562aabaa1
LUsername <492254446@qq.com>
2019-11-29 15:05:00 +0800

    引入axios，Ajax获取Header组件数据

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,30 +1,43 @@
 import React,{Component,Fragment} from 'react';
 import logo from './logo.png';
+import axios from 'axios';
 import './style.css';
 import { Menu, Icon } from 'antd';
 
 class AppHeader extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            list:[]
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://www.dell-lee.com/react/api/header.json')
+        .then((res)=>{
+            this.setState({
+                list:res.data.data
+            })
+        });
+    }
+
+    getMenuItems(){
+        return this.state.list.map(item =>{
+            return(
+                <Menu.Item key={item.id}>
+                    <Icon type={item.icon} />
+                    {item.title}    
+                </Menu.Item>
+            )
+        })
+    }
+
     render(){
         return(
             <Fragment>
                 <img src={logo} className="app-header-logo"/>
                 <Menu mode="horizontal" className="app-header-menu">
-                    <Menu.Item key="mail1">
-                        <Icon type="mail" />
-                        VOA慢速英语    
-                    </Menu.Item>
-                    <Menu.Item key="mail2">
-                        <Icon type="mail" />
-                        VOA慢速英语    
-                    </Menu.Item>
-                    <Menu.Item key="mail3">
-                        <Icon type="mail" />
-                        VOA慢速英语    
-                    </Menu.Item>
-                    <Menu.Item key="mail4">
-                        <Icon type="mail" />
-                        VOA慢速英语    
-                    </Menu.Item>
+                    {this.getMenuItems()}
                 </Menu>
             </Fragment>
         );
