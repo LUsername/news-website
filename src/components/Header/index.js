3158a92bc8078b9f49d9c3515f65dfcf368fc436
LUsername <492254446@qq.com>
2019-11-29 14:38:07 +0800

    Header组件拆分及样式布局（2）

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,32 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
 import logo from './logo.png';
+import './style.css';
+import { Menu, Icon } from 'antd';
 
 class AppHeader extends Component{
     render(){
         return(
-            <div>
-                <img src={logo} />
-            </div>
+            <Fragment>
+                <img src={logo} className="app-header-logo"/>
+                <Menu mode="horizontal" className="app-header-menu">
+                    <Menu.Item key="mail1">
+                        <Icon type="mail" />
+                        VOA慢速英语    
+                    </Menu.Item>
+                    <Menu.Item key="mail2">
+                        <Icon type="mail" />
+                        VOA慢速英语    
+                    </Menu.Item>
+                    <Menu.Item key="mail3">
+                        <Icon type="mail" />
+                        VOA慢速英语    
+                    </Menu.Item>
+                    <Menu.Item key="mail4">
+                        <Icon type="mail" />
+                        VOA慢速英语    
+                    </Menu.Item>
+                </Menu>
+            </Fragment>
         );
     }
 }
