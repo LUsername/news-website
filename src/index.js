f4c0e08b8e04faa99f52fc28420cc9c831034f9f
LUsername <492254446@qq.com>
2019-11-29 15:45:05 +0800

    Content区路由配置

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React,{Component} from 'react';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import AppHeader from './components/Header/index';
+import Detail from './containers/Detail';
+import List from './containers/List';
 import 'antd/dist/antd.css';
 import './style.css';
 import { Layout } from 'antd';
@@ -13,7 +16,14 @@ class App extends Component{
             <div>
                 <Layout style={{minWidth:1300}}>
                     <Header className="header"><AppHeader/></Header>
-                    <Content className="content">Content</Content>
+                    <Content className="content">
+                        <BrowserRouter>
+                            <Switch>
+                                <Route path='/detail' component={Detail}></Route>
+                                <Route path='/' component={List}></Route>
+                            </Switch>
+                        </BrowserRouter>
+                    </Content>
                     <Footer className="footer">Footer</Footer>
                 </Layout>
             </div>
