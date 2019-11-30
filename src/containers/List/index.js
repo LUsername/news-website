fb5eb857b106d18cf9995faace241e6a7c987356
LUsername <492254446@qq.com>
2019-11-30 09:47:43 +0800

    获取列表后台数据，更改footer区内容及样式

--- src/containers/List/index.js
+++ src/containers/List/index.js
@@ -1,26 +1,34 @@
 import React,{Component} from 'react';
 import { List } from 'antd';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import axios from 'axios';
 
 class NewList extends Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
 
     render(){
         return(
             <List
                 style={{background:'#fff'}}
                 bordered
-                dataSource={data}
-                renderItem={item => <List.Item>{item}</List.Item>}
+                dataSource={this.state.data}
+                renderItem={item => <List.Item>{item.title}</List.Item>}
             />
         )
+    }
+
+    componentDidMount(){
+        axios.get('http://www.dell-lee.com/react/api/list.json')
+        .then((res)=>{
+            this.setState({
+                data:res.data.data
+            });
+        })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -12,21 +12,20 @@ const { Header, Footer, Content } = Layout;
 
 class App extends Component{
     render(){
-        return(
-            <div>
-                <Layout style={{minWidth:1300}}>
-                    <Header className="header"><AppHeader/></Header>
-                    <Content className="content">
-                        <BrowserRouter>
-                            <Switch>
-                                <Route path='/detail' component={Detail}></Route>
-                                <Route path='/' component={List}></Route>
-                            </Switch>
-                        </BrowserRouter>
-                    </Content>
-                    <Footer className="footer">Footer</Footer>
-                </Layout>
-            </div>
+        return(   
+           <Layout style={{minWidth:1300, height:'100%'}}>
+               <Header className="header"><AppHeader/></Header>
+               <Content className="content">
+                   <BrowserRouter>
+                       <Switch>
+                           <Route path='/detail' component={Detail}></Route>
+                           <Route path='/' component={List}></Route>
+                       </Switch>
+                   </BrowserRouter>
+               </Content>
+               <Footer className="footer">@copyright Aaron 2019</Footer>
+           </Layout>
+    
         )
     }
 }
